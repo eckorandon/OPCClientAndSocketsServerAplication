aba08f371a519bfe87b3496f73c99e7f61f7eec5
Estevão Coelho Kiel de Oliveira <estevaokiel@gmail.com>
2021-08-06 19:34:10 -0300

    Major
    Update our OPC client with the sockets server.

--- SimpleOPCClient_VS_Community/SimpleOPCClient_v3.cpp
+++ SimpleOPCClient_VS_Community/SimpleOPCClient_v3.cpp
@@ -11,11 +11,14 @@
 
 	Alunos:
 	Estevao Coelho Kiel de Oliveira     - 2016119416
-	Lucas								- 
+	Lucas Costa Souza					- 2018013763
 
 	Data: Agosto de 2021
 
-	Explicacao do programa aqui
+	Aplicação multi thread que tem como objetivo a integracao de um servidor de sockets
+	e um cliente OPC. O servidor de sockets de comunica com um cliente de sockets
+	instalado em outra maquina de uma mesma rede e o cliente OPC se comunica com um
+	servidor OPC.
 */
 
 // Para a correta compilacao deste programa, nao se esqueca de incluir a
@@ -46,18 +49,36 @@
 // luizt at cpdee.ufmg.br
 //
 
+#undef UNICODE
+
+/* ======================================================================================================================== */
+/*  DEFINE AREA*/
+
+#define WIN32_LEAN_AND_MEAN
+#define OPC_SERVER_NAME			L"Matrikon.OPC.Simulation.1"
+#define VT						VT_R4
+
+#define DEFAULT_BUFLEN          64
+#define DEFAULT_PORT            "5447"
+
+#define TAMMSGSTATUS            42
+#define TAMMSGACK               10
+
+//#define REMOTE_SERVER_NAME L"your_path"
+
 /* ======================================================================================================================== */
 /*  INCLUDE AREA*/
 
-#include <atlbase.h>    // required for using the "_T" macro
+#include <atlbase.h>											/*required for using the "_T" macro*/
 #include <iostream>
 #include <ObjIdl.h>
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>		//	_getch()
+#include <conio.h>												/*_getch()*/
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <math.h>                                               /*pow()*/
 
 #include "opcda.h"
 #include "opcerror.h"
@@ -66,29 +87,17 @@
 #include "SOCDataCallback.h"
 #include "SOCWrapperFunctions.h"
 
-using namespace std;
-
 /* ======================================================================================================================== */
-/*  */
+/* OTHERS*/
+
+using namespace std;
 
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
 // #pragma comment (lib, "Mswsock.lib")
 
 /* ======================================================================================================================== */
-/*  DEFINE AREA*/
-
-#define WIN32_LEAN_AND_MEAN
-#define OPC_SERVER_NAME L"Matrikon.OPC.Simulation.1"
-#define VT VT_R4
-
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "5447"
-
-//#define REMOTE_SERVER_NAME L"your_path"
-
-/* ======================================================================================================================== */
-/*  DECLARACAO DO PROTOTIPO DE FUNCAO DAS THREADS SECUNDARIAS*/
+/* DECLARACAO DO PROTOTIPO DE FUNCAO DA THREAD SECUNDARIA*/
 
 DWORD WINAPI ServidorSockets(LPVOID index);
 
@@ -101,20 +110,21 @@ UINT OPC_DATA_TIME = RegisterClipboardFormat (_T("OPCSTMFORMATDATATIME"));
 
 wchar_t ITEM_ID[]=L"Saw-toothed Waves.Real4";
 
+int nseq = 0;                                                   /*Valor referente ao numero sequensial da mensagem*/
+
 /* ======================================================================================================================== */
 /*  THREAD PRIMARIA*/
 
 //////////////////////////////////////////////////////////////////////
 // Read the value of an item on an OPC server. 
 //
-void main(void)
-{
+void main(void) {
 	/*------------------------------------------------------------------------------*/
 	/*Nomeando terminal*/
 	SetConsoleTitle("TERMINAL PRINCIPAL");
 
 	/*------------------------------------------------------------------------------*/
-	/*Thread secundaria*/
+	/*Criando thread secundaria*/
 	HANDLE hServidorSockets;
 
 	DWORD dwServidorSocketsId;
@@ -283,9 +293,11 @@ void main(void)
 	printf ("Releasing the COM environment...\n");
 	CoUninitialize();
 
-	//close handles
+	/*------------------------------------------------------------------------------*/
+	/*Fecha handles*/
 	CloseHandle(hServidorSockets);
 }
+
 
 ////////////////////////////////////////////////////////////////////
 // Instantiate the IOPCServer interface of the OPCServer
@@ -354,6 +366,7 @@ void AddTheGroup(IOPCServer* pIOPCServer, IOPCItemMgt* &pIOPCItemMgt,
 	_ASSERT(!FAILED(hr));
 }
 
+
 //////////////////////////////////////////////////////////////////
 // Add the Item ITEM_ID to the group whose IOPCItemMgt interface
 // is pointed by pIOPCItemMgt pointer. Return a server opc handle
@@ -400,6 +413,7 @@ void AddTheItem(IOPCItemMgt* pIOPCItemMgt, OPCHANDLE& hServerItem)
 	pErrors = NULL;
 }
 
+
 ///////////////////////////////////////////////////////////////////////////////
 // Read from device the value of the item having the "hServerItem" server 
 // handle and belonging to the group whose one interface is pointed by
@@ -433,6 +447,7 @@ void ReadItem(IUnknown* pGroupIUnknown, OPCHANDLE hServerItem, VARIANT& varValue
 	pIOPCSyncIO->Release();
 }
 
+
 ///////////////////////////////////////////////////////////////////////////
 // Remove the item whose server handle is hServerItem from the group
 // whose IOPCItemMgt interface is pointed by pIOPCItemMgt
@@ -453,6 +468,7 @@ void RemoveItem(IOPCItemMgt* pIOPCItemMgt, OPCHANDLE hServerItem)
 	pErrors = NULL;
 }
 
+
 ////////////////////////////////////////////////////////////////////////
 // Remove the Group whose server handle is hServerGroup from the server
 // whose IOPCServer interface is pointed by pIOPCServer
@@ -469,30 +485,41 @@ void RemoveGroup (IOPCServer* pIOPCServer, OPCHANDLE hServerGroup)
 	}
 }
 
+
 ////////////////////////////////////////////////////////////////////////
 //
 //
-
 DWORD WINAPI ServidorSockets(LPVOID index) {
+	/*------------------------------------------------------------------------------*/
+	/*Declarando variaveis locais*/
+	WSADATA             wsaData;
 
-	WSADATA wsaData;
-	int iResult;
-
-	SOCKET ListenSocket = INVALID_SOCKET;
-	SOCKET ClientSocket = INVALID_SOCKET;
+	SOCKET              ListenSocket = INVALID_SOCKET;
+	SOCKET              ClientSocket = INVALID_SOCKET;
 
 	struct addrinfo* result = NULL;
-	struct addrinfo hints;
+	struct addrinfo     hints;
 
-	int iSendResult;
-	char recvbuf[DEFAULT_BUFLEN];
-	int recvbuflen = DEFAULT_BUFLEN;
+	SYSTEMTIME          SystemTime;
 
-	// Initialize Winsock
+	int                 iResult,
+		iSendResult,
+		k = 0,
+		recvbuflen = DEFAULT_BUFLEN;
+
+	char                recvbuf[DEFAULT_BUFLEN],
+		msgstatus[TAMMSGSTATUS + 1] = "99/#######/##/####.#/####.#/#####",
+		msgack[TAMMSGACK + 1] = "22/#######";
+
+	/*------------------------------------------------------------------------------*/
+	/*Configuracoes do servidor*/
+
+	/*Carrega a biblioteca winsock versao 2.2*/
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0) {
-		printf("WSAStartup failed with error: %d\n", iResult);
-		return 1;
+		printf("Falha na inicializacao do Winsock 2! Erro = %d\n", WSAGetLastError());
+		WSACleanup();
+		exit(0);
 	}
 
 	ZeroMemory(&hints, sizeof(hints));
@@ -501,96 +528,153 @@ DWORD WINAPI ServidorSockets(LPVOID index) {
 	hints.ai_protocol = IPPROTO_TCP;
 	hints.ai_flags = AI_PASSIVE;
 
-	// Resolve the server address and port
+	/*Definindo o endereco do servidor e porta para conexao*/
 	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
 	if (iResult != 0) {
-		printf("getaddrinfo failed with error: %d\n", iResult);
+		printf("Falha em getaddrinfo()! Erro = %d\n", WSAGetLastError());
 		WSACleanup();
-		return 1;
+		exit(0);
 	}
 
-	// Create a SOCKET for connecting to server
+	/*Criacao do socket para conexao com o servidor*/
 	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 	if (ListenSocket == INVALID_SOCKET) {
-		printf("socket failed with error: %ld\n", WSAGetLastError());
+		printf("Falha na inicializacao do socket! Erro = %d\n", WSAGetLastError());
 		freeaddrinfo(result);
 		WSACleanup();
-		return 1;
+		exit(0);
 	}
 
-	// Setup the TCP listening socket
+	/*Vincula o socket a porta definida*/
 	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR) {
-		printf("bind failed with error: %d\n", WSAGetLastError());
+		printf("Falha na vinculacao do socket a porta TCP! Erro = %d\n", WSAGetLastError());
 		freeaddrinfo(result);
 		closesocket(ListenSocket);
 		WSACleanup();
-		return 1;
+		exit(0);
 	}
 
 	freeaddrinfo(result);
 
-	iResult = listen(ListenSocket, SOMAXCONN);
+	/*Colocando a porta TCP em modo de escuta - Backlog de 5 eh o valor tipico*/
+	iResult = listen(ListenSocket, 5);
 	if (iResult == SOCKET_ERROR) {
-		printf("listen failed with error: %d\n", WSAGetLastError());
+		printf("Falha ao colocar a porta TCP no modo de escuta! Erro = %d\n", WSAGetLastError());
 		closesocket(ListenSocket);
 		WSACleanup();
-		return 1;
+		exit(0);
 	}
 
-	// Accept a client socket
+	/*Aguarda a conexao de um cliente de sockets entao aceita*/
 	ClientSocket = accept(ListenSocket, NULL, NULL);
-	printf("1\n");
 	if (ClientSocket == INVALID_SOCKET) {
-		printf("accept failed with error: %d\n", WSAGetLastError());
+		printf("Falha ao aceitar a conexao com o cliente de sockets! Erro = %d\n", WSAGetLastError());
 		closesocket(ListenSocket);
 		WSACleanup();
-		return 1;
+		exit(0);
 	}
-	printf("2\n");
 
-	// No longer need server socket
-	closesocket(ListenSocket);
-
-	// Receive until the peer shuts down the connection
+	/*Recebe e envia mensagens ate que a conexao seja encerrada*/
 	do {
-		printf("3\n");
 		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
 		if (iResult > 0) {
-			printf("Bytes received: %d\n", iResult);
 
-			// Echo the buffer back to the sender
-			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
-			if (iSendResult == SOCKET_ERROR) {
-				printf("send failed with error: %d\n", WSAGetLastError());
-				closesocket(ClientSocket);
-				WSACleanup();
-				return 1;
+			/*Exibe a hora corrente*/
+			GetSystemTime(&SystemTime);
+			printf("SISTEMA DE CONTROLE: data/hora local = %02d-%02d-%04d %02d:%02d:%02d\n",
+				SystemTime.wDay, SystemTime.wMonth, SystemTime.wYear,
+				SystemTime.wHour, SystemTime.wMinute, SystemTime.wSecond);
+
+			/*Caso tenha solicitado status da planta*/
+			if (iResult == 10) {
+
+				/*Imprime mensagem recebida em verde*/
+				printf("\x1b[32m");
+				printf("Msg de requisicao de dados recebida do MES\n%.10s\n\n", recvbuf);
+				printf("\x1b[0m");
+
+				nseq = (nseq + 2) % 9999999;
+
+				for (int j = 3; j < 10; j++) {
+					k = nseq / pow(10, (9 - j));
+					k = k % 10;
+					msgstatus[j] = k + '0';
+				}
+
+				/*Envia mensagem com dados do status da planta*/
+				iSendResult = send(ClientSocket, msgstatus, TAMMSGSTATUS, 0);
+				if (iSendResult == SOCKET_ERROR) {
+					printf("Falha ao enviar a mensagem com dados do status da planta! Erro = %d\n", WSAGetLastError());
+					closesocket(ClientSocket);
+					WSACleanup();
+					exit(0);
+				}
+
+				/*Imprime mensagem enviada em amarelo*/
+				printf("\x1b[33m");
+				printf("Mensagem de status da planta enviada ao MES\n%s\n\n", msgstatus);
+				printf("\x1b[0m");
 			}
-			printf("Bytes sent: %d\n", iSendResult);
+
+			/*Caso tenha solicitado confirmcao*/
+			if (iResult == 33) {
+
+				/*Imprime mensagem recebida em cyan*/
+				printf("\x1b[36m");
+				printf("Mensagem de setup de quipamentos recebida do MES\n%.33s\n\n", recvbuf);
+				printf("\x1b[0m");
+
+				nseq = (nseq + 2) % 9999999;
+
+				for (int j = 3; j < 10; j++) {
+					k = nseq / pow(10, (9 - j));
+					k = k % 10;
+					msgack[j] = k + '0';
+				}
+
+				/*Envia mensagem de confirmacao (ACK)*/
+				iSendResult = send(ClientSocket, msgack, TAMMSGACK, 0);
+				if (iSendResult == SOCKET_ERROR) {
+					printf("Falha ao enviar a mensagem de confirmacao (ACK)! Erro = %d\n", WSAGetLastError());
+					closesocket(ClientSocket);
+					WSACleanup();
+					exit(0);
+				}
+
+				/*Imprime mensagem enviada em magenta*/
+				printf("\x1b[35m");
+				printf("Mensagem de ACK enviada ao MES\n%s\n\n", msgack);
+				printf("\x1b[0m");
+			}
 		}
-		else if (iResult == 0)
-			printf("Connection closing...\n");
-		else {
-			printf("recv failed with error: %d\n", WSAGetLastError());
+		else if (iResult == 0) {
+			printf("Nenhum dado recebi, encerrando servidor de sockets!");
+		}
+		else if (iResult == SOCKET_ERROR) {
+			printf("Falha ao receber dados do cliente sockets - recv()! Erro = %d\n", WSAGetLastError());
 			closesocket(ClientSocket);
 			WSACleanup();
-			return 1;
+			exit(0);
 		}
-
 	} while (iResult > 0);
 
-	// shutdown the connection since we're done
+	/*Fechando o socket*/
+	closesocket(ListenSocket);
+
+	/*Desligando a conexao*/
 	iResult = shutdown(ClientSocket, SD_SEND);
 	if (iResult == SOCKET_ERROR) {
-		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		printf("Falha ao desligar a conexao! Erro = %d\n", WSAGetLastError());
 		closesocket(ClientSocket);
 		WSACleanup();
-		return 1;
+		exit(0);
 	}
 
-	// cleanup
+	/*Fechando o socket*/
 	closesocket(ClientSocket);
+
+	/*Cleanup*/
 	WSACleanup();
 
 	/*------------------------------------------------------------------------------*/
